4c5ae8aabfa4cbfe2dd5fd8607fc7cb53eb6cc6a
Daniele Trapani <daniele.trapani96@gmail.com>
2023-08-21 16:21:22 +0200

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'boats#index'
+
+  #   resources :boats
+  resources :bookings, only: %i[destroy edit update]
+
+  resources :boats, except: %i[index] do
+    resources :bookings, only: %i[new create]
+  end
 end
