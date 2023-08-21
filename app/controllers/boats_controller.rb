0eb7afbebb8df2b8448238cbe38ede40e7133460
Cristian Goenaga <crisgoera@gmail.com>
2023-08-21 16:19:42 +0200

    Boats controller

---
+++ app/controllers/boats_controller.rb
@@ -0,0 +1,36 @@
+class BoatsController < ApplicationController
+
+  def index
+    @boats = Boat.all
+  end
+
+  def show
+    @boat = Boat.find(params[:id])
+  end
+
+  def new
+    @boat = Boat.new
+  end
+
+  def create
+    @boat = Boat.new(boat_params)
+    @boat.save
+
+    redirect_to action: 'index'
+  end
+
+  def edit
+    @boat = Boat.find(params[:id])
+  end
+
+  def destroy
+    @boat = Boat.find(params[:id])
+    @boat.destroy
+  end
+
+  private
+
+  def boat_params
+    params.require(:boat).permit(:id)
+  end
+end
