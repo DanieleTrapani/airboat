c41db9d8beef871612ff475558055112d1215829
Daniele Trapani <daniele.trapani96@gmail.com>
2023-08-21 17:02:36 +0200

    bookings_controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,34 @@
 class BookingsController < ApplicationController
+  def new
+    @boat = Boat.find(params[:boat_id])
+    @booking = Booking.new
+  end
+
+  def create
+    @boat = Boat.find(params[:boat_id])
+    booking = Booking.new(booking_params)
+    booking.boat = @boat
+    booking.user = current_user
+    booking.save
+  end
+
+  def edit
+    @booking = Booking.find(params[:id])
+  end
+
+  def update
+    booking = Booking.find(params[:id])
+    booking.update(booking_params)
+  end
+
+  def destroy
+    id = params[:id]
+    Booking.destroy(id)
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date)
+  end
 end
