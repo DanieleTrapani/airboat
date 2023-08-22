c4c4cd282b0f434c921da3b44eda4ef60eb16267
Cristian Goenaga <crisgoera@gmail.com>
2023-08-22 11:42:48 +0200

    Update to seeding

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :boats
-  has_many :bookings
+  has_many :boats, dependent: :destroy
+  has_many :bookings, dependent: :destroy
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,8 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
+User.destroy_all
+
 10.times do
   # User generation
   first_name = Faker::Name.first_name
