bdd96f6f3e38fa3e0eed7d02f1930ca9009bb0b9
Cristian Goenaga <crisgoera@gmail.com>
2023-08-22 15:07:30 +0200

    Making new booking page

---
+++ app/controllers/date_controller.js
@@ -0,0 +1,12 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  
+  connect() {
+    console.log("Date controller properly connected")
+  }
+
+  day_count() {
+
+  }
+}
