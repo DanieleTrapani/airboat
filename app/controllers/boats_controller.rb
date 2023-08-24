a77747ae9d49a64d5a8407c8c69e06ceba5e8d6c
Nancy Qian <luyao.q@gmail.com>
2023-08-24 15:21:12 +0200

    added delete

--- app/controllers/boats_controller.rb
+++ app/controllers/boats_controller.rb
@@ -17,7 +17,9 @@ class BoatsController < ApplicationController
     @boat = Boat.new(boat_params)
     @boat.user_id = current_user.id
     # TODO: Temporary solution to avoid breaking
-    @boat.img_url = "https://loremflickr.com/320/240/yacht?#{@boat.name}"
+    # @boat.img_url = "https://loremflickr.com/320/240/yacht?#{@boat.name}"
+    @boat.img_url = "https://images.unsplash.com/photo-1600177897995-e67c73ed44e9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzB8fHlhY2h0fGVufDB8fDB8fHww&auto=format&fit=crop&w=1000&q=60"
+
     if @boat.save
       redirect_to boat_path(@boat)
     else
@@ -32,6 +34,7 @@ class BoatsController < ApplicationController
   def destroy
     @boat = Boat.find(params[:id])
     @boat.destroy
+    redirect_to dashboard_path, status: :see_other
   end
 
   private
